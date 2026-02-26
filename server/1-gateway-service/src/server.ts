eae82c94836496e57e5fb6595c5c2a2bf90022c5
19010853 <khoinguyenminhk37@gmail.com>
2026-02-26 09:34:12 +0700

    feat: gateway server and app file

---
+++ server/1-gateway-service/src/server.ts
@@ -0,0 +1,112 @@
+import { Logger } from "winston";
+import { CustomError, IErrorResponse, winstonLogger } from '@19010853/ithust-shared';
+import { Application, json, urlencoded } from "express";
+import cookieSession from "cookie-session";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import http from 'http';
+import { isAxiosError } from "axios";
+
+const DEFAULT_ERROR_CODE = 500;
+
+
+const SERVER_PORT = 4000;
+const log: Logger = winstonLogger('', 'gatewayServer', 'debug');
+
+export class GatewayServer {
+    private app: Application;
+
+    constructor(app: Application) {
+        this.app = app;
+    }
+
+    public start(): void {
+        this.securityMiddleware(this.app);
+        this.standardMiddleware(this.app);
+        this.routesMiddleware(this.app);
+        this.startElasticSearch();
+        this.errorHandler(this.app);
+        this.startServer(this.app);
+    }
+
+    private securityMiddleware(app: Application): void {
+        app.set('trust proxy', 1);
+        app.use(
+            cookieSession({
+                name: 'session',
+                keys: [],
+                maxAge: 24 * 7 * 3600000,
+                secure: false // update with value from config
+            })
+        );
+        app.use(hpp());
+        app.use(helmet());
+        app.use(cors({
+            origin: '',
+            credentials: true,
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization']
+        }))
+    }
+
+    private standardMiddleware(app: Application): void {
+        app.use(compression());
+        app.use(json({ limit: '200mb' }));
+        app.use(urlencoded({ extended: true, limit: '200mb' }));
+    }
+
+    private routesMiddleware(app: Application): void {
+
+    }
+
+    private startElasticSearch(): void {
+
+    }
+
+    private errorHandler(app: Application): void {
+        app.use('*', (req: Request, res: Response, next: NextFunction) => {
+            const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+            log.log('error', `${fullUrl} endpoint does not exist.`, '');
+            res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.' });
+            next();
+        });
+
+        app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+            if (error instanceof CustomError) {
+                log.log('error', `GatewayService ${error.comingFrom}:`, error);
+                res.status(error.statusCode).json(error.serializedErrors());
+            }
+
+            if (isAxiosError(error)) {
+                log.log('error', `GatewayService Axios Error - ${error?.response?.data?.comingFrom}:`, error);
+                res.status(error?.response?.data?.statusCode ?? DEFAULT_ERROR_CODE).json({ message: error?.response?.data?.message ?? 'Error occurred.' });
+            }
+
+            next();
+        });
+    }
+
+    private async startServer(app: Application): Promise<void> {
+        try {
+            const httpServer: http.Server = new http.Server(app);
+            this.startHttpServer(httpServer);
+        } catch (error) {
+            log.log('error', 'GatewayService startServer() error method:', error);
+        }
+    }
+
+    private async startHttpServer(httpServer: http.Server): Promise<void> {
+        try {
+            log.info(`Gateway server has started with process id ${process.pid}`);
+            httpServer.listen(SERVER_PORT, () => {
+                log.info(`Gateway server running on port ${SERVER_PORT}`);
+            });
+        } catch (error) {
+            log.log('error', 'GatewayService startServer() error method:', error);
+        }
+    }
+}
