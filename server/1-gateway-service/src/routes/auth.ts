17959d8557e455e0009366361a05f316c935a628
19010853 <khoinguyenminhk37@gmail.com>
2026-02-28 16:14:39 +0700

    feat: add verify email controller for both gateway and auth service

--- server/1-gateway-service/src/routes/auth.ts
+++ server/1-gateway-service/src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { SignUp } from "@gateway/controllers/auth/signup";
 import { SignIn } from "@gateway/controllers/auth/signin";
+import { VerifyEmail } from "@gateway/controllers/auth/verify-email";
 import { Router } from "express";
 
 class AuthRoutes {
@@ -12,6 +13,7 @@ class AuthRoutes {
     public routes(): Router {
         this.router.post('/auth/signup', SignUp.prototype.create);
         this.router.post('/auth/signin', SignIn.prototype.read);
+        this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
         return this.router;
     }
 }
--- server/3-auth-service/src/routes/auth.ts
+++ server/3-auth-service/src/routes/auth.ts
@@ -1,7 +1,7 @@
 // import { changePassword, forgotPassword, resetPassword } from '@auth/controllers/password';
-// import { read } from '@auth/controllers/signin';
+import { read } from '@auth/controllers/signin';
 import { create } from '@auth/controllers/signup';
-// import { update } from '@auth/controllers/verify-email';
+import { update } from '@auth/controllers/verify-email';
 // import { updateOTP } from '@auth/controllers/verify-otp';
 import express, { Router } from 'express';
 
@@ -9,8 +9,8 @@ const router: Router = express.Router();
 
 export function authRoutes(): Router {
     router.post('/signup', create);
-    //   router.post('/signin', read);
-    //   router.put('/verify-email', update);
+    router.post('/signin', read);
+    router.put('/verify-email', update);
     //   router.put('/verify-otp/:otp', updateOTP);
     //   router.put('/forgot-password', forgotPassword);
     //   router.put('/reset-password/:token', resetPassword);
