e1a1f04616a7a241fea97090c1e10b1f45e5e997
19010853 <khoinguyenminhk37@gmail.com>
2026-03-01 08:55:54 +0700

    add search routes and controllers for gateway and auth service

--- server/1-gateway-service/src/routes.ts
+++ server/1-gateway-service/src/routes.ts
@@ -1,10 +1,12 @@
 import { Application } from 'express';
 import { healthRoutes } from '@gateway/routes/health';
 import { authRoutes } from '@gateway/routes/auth';
+import { searchRoutes } from '@gateway/routes/search';
 
 const BASE_PATH = '/api/gateway/v1';
 
 export const appRoutes = (app: Application) => {
     app.use(`${BASE_PATH}`, healthRoutes.routes());
     app.use(`${BASE_PATH}`, authRoutes.routes());
+    app.use(`${BASE_PATH}`, searchRoutes.routes());
 };
