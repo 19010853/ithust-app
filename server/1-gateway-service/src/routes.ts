d662f576727c315a43ac4983c8858f33f705fed3
19010853 <khoinguyenminhk37@gmail.com>
2026-03-02 15:20:04 +0700

    feat: add seller controllers for both gateway and users services

--- server/1-gateway-service/src/routes.ts
+++ server/1-gateway-service/src/routes.ts
@@ -2,6 +2,10 @@ import { Application } from 'express';
 import { healthRoutes } from '@gateway/routes/health';
 import { authRoutes } from '@gateway/routes/auth';
 import { searchRoutes } from '@gateway/routes/search';
+import { buyerRoutes } from '@gateway/routes/buyer';
+import { sellerRoutes } from './routes/seller';
+import { currentUserRoutes } from './routes/current-user';
+import { authMiddleware } from './services/auth-middleware';
 
 const BASE_PATH = '/api/gateway/v1';
 
@@ -9,4 +13,7 @@ export const appRoutes = (app: Application) => {
     app.use(`${BASE_PATH}`, healthRoutes.routes());
     app.use(`${BASE_PATH}`, authRoutes.routes());
     app.use(`${BASE_PATH}`, searchRoutes.routes());
+    app.use(`${BASE_PATH}`, buyerRoutes.routes());
+    app.use(`${BASE_PATH}`, sellerRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
 };
